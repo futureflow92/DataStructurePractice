5f717f06a33b80e5d26e3a8ae106e751648a372a
futureflow92 <futureflow92@hotmail.com>
2022-10-06 22:33:31 +0900

    2. Linked List 중복값 삭제하는 메서드 추가

--- src/main/java/com/datastructure/datastructure/linkedlist/LinkedListNode.java
+++ src/main/java/com/datastructure/datastructure/linkedlist/LinkedListNode.java
@@ -43,19 +43,33 @@ class LinkedList {
         }
         System.out.println(n.data);
     }
+
+    //[자료구조 알고리즘] Linked List 중복값 삭제 in Java
+    void removeDups() {
+        Node n = header;
+        //마지막 값이 중복값이고 r이 그 중복값을 제거하고나면 n은 마지막에 도달했을 때 null이 되고 에러가 발생한다.
+        while(n!=null && n.next!=null) {
+            Node r = n;
+            while(r.next != null){
+                if(n.data == r.next.data){
+                    r.next = r.next.next;
+                } else {
+                    r = r.next;
+                }
+            }
+            n = n.next;
+        }
+    }
 }
 
 public class LinkedListNode {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.append(1);
         ll.append(2);
-        ll.append(3);
-        ll.append(4);
-        ll.retrieve();
-        ll.delete(1);
+        ll.append(2);
         ll.retrieve();
 
-
+        ll.removeDups();
+        ll.retrieve();
     }
 }
